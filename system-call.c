9917c2ce830c9f418dd0bdc79802e3d2b84c99cd
Ana Diedrichs <ana.diedrichs@frm.utn.edu.ar>
2019-10-18 06:04:24 -0300

    scanf fix

--- system-call.c
+++ system-call.c
@@ -1,9 +1,35 @@
+/*
+  __                       .__              .__   
+_/  |_  ___________  _____ |__| ____ _____  |  |  
+\   __\/ __ \_  __ \/     \|  |/    \\__  \ |  |  
+ |  | \  ___/|  | \/  Y Y  \  |   |  \/ __ \|  |__
+ |__|  \___  >__|  |__|_|  /__|___|  (____  /____/
+           \/            \/        \/     \/      
+         _nnnn_                      
+        dGGGGMMb     ,"""""""""""""".
+       @p~qp~~qMb    | Linux Rules! |
+       M|@||@) M|   _;..............'
+       @,----.JM| -'
+      JS^\__/  qKL
+     dZP        qKRb
+    dZP          qKKb
+   fZP            SMMb
+   HZM            MMMM
+   FqM            MMMM
+ __| ".        |\dS"qML
+ |    `.       | `' \Zq
+_)      \.___.,|     .'
+\____   )MMMMMM|   .'
+     `-'       `--' hjm
+
+
+*/
+
 //<stdlib.h> library has system() library function. 
 #include<stdlib.h> 
 // por printf y scanf 
 #include <stdio.h>
-#define SALIR 0
-#define COMPILAR 
+
 
 void mostrar_menu()
 {
@@ -15,40 +41,64 @@ void mostrar_menu()
 	printf("0 - Salir\n");
 }
 
+int obtener_opcion()
+{
+		char o[10]; int opcion;
+
+		scanf("%c",o);
+		
+		opcion = atoi(o);
+
+		printf("Ud. eligió %d \n",opcion);	
+}
+
 
 int main(void)
 {
 
 	printf("Levantando otros programas\n");
 	int opcion = 1;
+	pid_t  pid;
 
-	while(opcion != SALIR)
+	while(opcion != 0)
 	{
 		
 		mostrar_menu();
+		int opcion = obtener_opcion();
+
 		scanf("%d",&opcion);
 		
 		printf("Ud. eligió %d \n",opcion);	
 
 		switch(opcion) {
 
-		   case SALIR  :
-			   break; 
+		   case 0  :
+				return 0; break; 
 			
 		   case 1  :
-			  system("gnome-calculator");
-			  break; 
+				system("gnome-calculator");
+				break; 
 		   case 2  :
 			  system("clear");
 			  break;
  		   case 3  :
 			  system("ifconfig");
 			  break;
-		   /* you can have any number of case statements */
+		   case 4  :
+				system("gedit");
+				break;
+			   
 		   default : 
 			printf("Ingrese una opción válida  \n");	
 		   
-		}
+		}// fin switch
+
+		// si es proceso hijo,
+		// que ejecute en proceso aparte
+
+		//si es hijo, cerrar proceso
+		if (pid == 0) break; // salir del while, luego salir del programa.
+
 	} // fin while 
     return 0;
 }
